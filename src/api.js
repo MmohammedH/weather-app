d337d4567aea9cb60017be0fea3f30ef7346bf47
MmohammedH <131524555+MmohammedH@users.noreply.github.com>
2024-05-04 19:43:30 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+    method: 'GET',
+    url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities',
+    headers: {
+      'X-RapidAPI-Key': '756534b296msh9b94a14977fd7adp123c71jsn7cda0e20fb5b',
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "19b2cc4b4fab037997fad163f02ea488";
